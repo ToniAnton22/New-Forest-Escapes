f7a89370fd5e1e9fc11cdfcd99acfa46a68faed9
Tyroneekhator <72547969+Tyroneekhator@users.noreply.github.com>
2023-03-03 20:09:39 +0000

    tyrones schema part

---
+++ backend/Model/Agency.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.schema;
+
+const agencySchema = new Schema({
+  agencyName: {
+    type: String,
+    required: true,
+  },
+  agentName: {
+    type: String,
+    required: true,
+  },
+  Readings:{
+    type:Number,
+    required: true,
+
+  },
+  Total_Cost:{
+    type: Number,
+    required: true,
+  }
+
+});
+
+module.exports = mongoose.model('Agency',agencySchema);
+
---
+++ backend/Model/Users.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.schema;
+
+const usersSchema = new Schema({
+  FirstName: {
+    type: String,
+    required: true,
+  },
+  LastName: {
+    type: String,
+    required: true,
+  },
+  delegated:{
+    type:String,
+    required:true,
+  }
+});
+
+module.exports = mongoose.model("Users", usersSchema);
