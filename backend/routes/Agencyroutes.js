bcc1d6de2a4d1670dbe0174efe800c79693ee184
ToniAnton22 <frincuanton@yahoo.com>
2023-03-25 22:29:40 +0200

    pushed changes

--- backend/routes/Agencyroutes.js
+++ backend/routes/Agencyroutes.js
@@ -1,22 +1,23 @@
 const express = require("express")
-const router = express.Router();
+const router = express.Router()
 const Agency = require("../Model/Agency")
 
-router.get("/add-agency", (req,res) =>{
-    const agency = new Agency({ 
-      agencyName: "TestAgencyname2",  //just to test, need to replased by .body
-      agentName: "TestAgentname",
-      Readings: "22",
-      Total_Cost: "11"
-    })
-  
-    agency.save()
-    .then((result) =>{
+router.get("/add-agency", (req, res) => {
+  const agency = new Agency({
+    agencyName: "TestAgencyname2", //just to test, need to replased by .body
+    agentName: "TestAgentname",
+    Readings: "22",
+    Total_Cost: "11",
+  })
+
+  agency
+    .save()
+    .then((result) => {
       res.send(result)
     })
     .catch((err) => {
       console.log(err)
     })
-  })
+})
 
-  module.exports = router
+module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const app = express()
 mongoose.set("strictQuery", false)
 const agencyroutes = require("./routes/Agencyroutes")
 
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(cors())
 app.use(express.json())
 
@@ -14,16 +14,10 @@ app.set("view engine", "ejs")
 
 //database connection
 
-mongoose.connect(process.env.MONGO_URI)
-    .then((result) => app.listen(5000))
-    .catch((err) => console.log(err))
-    
-    
-      
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then((result) => app.listen(5000))
+  .catch((err) => console.log(err))
 
-      
 // agency routes
 app.use(agencyroutes)
-
-
-
